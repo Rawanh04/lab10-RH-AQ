ccd09deaf7e5baf2c6d6942a08bfbbdb92f7815d
Rawanh04 <rawanahussain04@gmail.com>
2025-04-13 20:25:00 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -48,10 +48,20 @@ def divide(a,b):
         raise ZeroDivisionError
 
 def logarithm(a, b):
-    try:
-        return math.log(b,a)
-    except ValueError:
+    if a <= 0 or b <= 0:
         raise ValueError
+    return math.log(b, a)
+
 
 def exponent(a,b):
     return a**b
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
